fc1c02f32d553603974e3d8cbca54fbfe7ee3e64
Ilya <ilya.kovalev001@gmail.com>
2023-10-29 17:17:44 +0300

    [+]: pagination module add

---
+++ util/pagination/pagination.py
@@ -0,0 +1,13 @@
+from django.core.paginator import Paginator
+
+
+def paginate(request, objects, per_page=10):
+
+    page_number = request.GET.get("page")
+
+    if page_number == None:
+        page_number = 1
+    
+    paginator = Paginator(objects, per_page)
+
+    return paginator.page(page_number)
