44fe27e4bb1e919c9914a160de141753dfa5bc64
Ilya <ilya.kovalev001@gmail.com>
2023-12-07 00:40:50 +0300

    [+]: POST forms handling add

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.template import loader
 from util.pagination.pagination import paginate
 from util.mock.mock import *
-from blog.models import Question, Answer, Profile, Tag
+from blog.models import Question, Answer, Profile, Tag, User
 from django.core.cache import cache
+from django.contrib import auth
+from django.urls import reverse
+from blog.forms import LoginForm, SignupForm, SettingsForm, AskForm
+from django.contrib.auth.decorators import login_required
 
 
 def add_context(context: dict) -> None:
@@ -33,8 +37,7 @@ def new(request: HttpRequest) -> HttpResponse:
         "questions": pages,
         "title": "New Questions",
         "subtitle": "Hot Questions",
-        "loggedIn": 1,
-        "name": "Saul Goodman"
+        # "name": "Saul Goodman"
     }
     add_context(context)
 
@@ -58,8 +61,7 @@ def hot(request: HttpRequest) -> HttpResponse:
     context = {
         "questions": pages, 
         "title": "Hot Questions",
-        "loggedIn": 1,
-        "name": "Saul Goodman"
+        # "name": "Saul Goodman"
     }
     add_context(context)
 
@@ -88,8 +90,7 @@ def tag(request: HttpRequest, tag: str) -> HttpResponse:
     context = {
         "questions": pages,
         "title": title,
-        "loggedIn": 1,
-        "name": "Saul Goodman"
+        # "name": "Saul Goodman"
     }
     add_context(context)
     
@@ -116,8 +117,7 @@ def question(request: HttpRequest, id: int) -> HttpResponse:
         "question": question,
         "answers": pages,
         "title": f"Question {id}",
-        "name": "Saul Goodman",
-        "loggedIn": 1
+        # "name": "Saul Goodman",
     }
     add_context(context)
 
@@ -130,38 +130,106 @@ def question(request: HttpRequest, id: int) -> HttpResponse:
 
 
 def login(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect("/")
+    
     template = loader.get_template('login.html')
+
+    # print("GET", request.GET)
+    # print("POST", request.POST)
+
+    login_form = LoginForm()
+
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(request, **login_form.cleaned_data)
+
+            if user:
+                auth.login(request, user)
+                return redirect(request.POST.get("continue", "/"))
+            else:
+                login_form.add_error(field="password", error="Wrong password")
 
     return HttpResponse(
         template.render(
             {
-                "loggedIn": 0
+                'login_form': login_form,
             },
-            request
+            request=request
         )
     )
+
+
+def logout(request: HttpRequest) -> HttpResponse:
+    auth.logout(request)
+    return redirect(reverse('login-page'))
 
 
 def signup(request: HttpRequest) -> HttpResponse:
     template = loader.get_template('signup.html')
 
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    signup_form = SignupForm()
+
+    if request.method == "POST":
+        print("POST:", request.POST)
+        signup_form = SignupForm(request.POST)
+        print("AFTER POST:", signup_form.data)
+
+        if signup_form.is_valid():
+            try:
+                signup_form.save()
+            except Exception as ex:
+                print(f"Ex: {ex}")
+                # signup_form.add_error(field="username", error="Such username already exists")
+
+            user = auth.authenticate(request, **signup_form.cleaned_data)
+            if user:
+                auth.login(request, user)
+                return redirect(request.POST.get("continue", "/"))
+            else:
+                signup_form.add_error("username", "Account wasn't created")
+
     return HttpResponse(
         template.render(
-            {
-                "loggedIn": 0
-            },
-            request
+            {'signup_form': signup_form,},
+            request=request
         )
     )
 
 
+@login_required(login_url="/login", redirect_field_name="continue")
 def ask(request: HttpRequest) -> HttpResponse:
     template = loader.get_template('ask.html')
+    # askForm = AskForm()
+
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            askForm = AskForm(request.POST)
+
+            if askForm.is_valid():
+                try:
+                    new_question = Question.objects.create(
+                        author=Profile.objects.filter(user=request.user)[0],
+                        title=askForm.cleaned_data['title'],
+                        description=askForm.cleaned_data['description'],
+                    )
+                    for tag in askForm.cleaned_data.get('tags', '').split(","):
+                        existing_tag = Tag.objects.get(word=tag.strip(" "))
+                        if existing_tag:
+                            new_question.tags.add(existing_tag)
+                        else:
+                            new_tag = Tag.objects.create(word=tag.strip(" "))
+                            new_question.tags.add(new_tag)
+                except Exception as ex:
+                    print(f"Question creation error: {ex}")
 
     context = {
-        "title": "New Question",
-        "name": "Saul Goodman",
-        "loggedIn": 1
+        'ask_form': AskForm(),
     }
     add_context(context)
 
@@ -173,14 +241,36 @@ def ask(request: HttpRequest) -> HttpResponse:
     )
 
 
+@login_required(login_url="/login", redirect_field_name="continue")
 def settings(request: HttpRequest) -> HttpResponse:
     template = loader.get_template('settings.html')
+    settingsForm = SettingsForm()
 
-    context = {
-        "username": "sAllGoodMan228",
-        "name": "Saul Goodman",
-        "loggedIn": 1
-    }
+    if request.user.is_authenticated:
+        settingsForm = SettingsForm(
+            data={
+                'email': request.user.email,
+                'username': request.user.username,
+                # 'avatar': request.user.profile.avatar,
+            } 
+        )
+
+        if request.method == "POST":
+            settingsForm = SettingsForm(request.POST)
+
+            if settingsForm.is_valid():
+                try:
+                    User.objects.filter(id=request.user.id).update(
+                        email=settingsForm.cleaned_data.get("email", ""),
+                        username=settingsForm.cleaned_data.get("username", ""),
+                    )
+                    Profile.objects.filter(id=request.user.id).update(
+                        avatar=settingsForm.cleaned_data.get("avatar", ""),
+                    )
+                except Exception as ex:
+                    print(f"Update error: {ex}")
+
+    context = {'settings_form': settingsForm}
     add_context(context)
     
     return HttpResponse(
